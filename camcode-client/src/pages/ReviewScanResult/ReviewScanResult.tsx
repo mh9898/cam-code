8d4c6295df66f0847205dc9802be493de2ef8ae2
MH <mh9898@gmail.com>
2022-06-02 11:38:36 +0300

    ScanResult - change result order fix UI

--- camcode-client/src/pages/ReviewScanResult/ReviewScanResult.tsx
+++ camcode-client/src/pages/ReviewScanResult/ReviewScanResult.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Layout from '@/layout/Layout';
-import { Card, Center, Text, Title } from '@mantine/core';
+import { Card, Center, Text, Title, Space } from '@mantine/core';
 import Button from '@/lib/Button/Button';
 import useCustomTheme from '@/hooks/useCustomTheme';
 import { log } from 'util';
@@ -13,14 +13,15 @@ const ReviewScanResult = () => {
 
   return (
     <Layout>
-      <Center sx={{ flexDirection: 'column', textAlign: 'center' }}>
+      <Center sx={{ flexDirection: 'column', textAlign: 'center'}}>
+     <Space h="xl" />
         <Text>{state.sQuality}</Text>
         <Card
           my="2rem"
           radius="xl"
-          sx={{ background: state?.cardBg, width: '90%', height: '25vh' }}
+          sx={{ background: state?.cardBg, width: '90%', height: '10vh' }}
         >
-          <Center sx={{ height: '100%', textAlign: 'center' }}>
+          <Center sx={{ height: '100%', textAlign: 'center'  }}>
             <Title order={3}>{state?.result}</Title>
           </Center>
         </Card>
